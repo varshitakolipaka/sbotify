ea5faba9b5e648ca598fda01e983cc46f259b363
varshitakolipaka <varshita2003@gmail.com>
2021-06-17 17:08:48 +0400

    whatevs

--- functions.py
+++ functions.py
@@ -8,14 +8,20 @@ scope = 'playlist-modify-public'
 username = '6tuo5kv4t1yadwyhogmk05jon' 
 
 token = SpotifyOAuth(scope=scope, username=username)
+print("HELLOOOO0")
 spotifyObject = spotipy.Spotify(auth_manager = token)
+print("HELLOOOO1")
 playlist_name = input("Add a playlist name ")
+print("HELLOOOO2")
 playlist_description = input("Say something about the playlist ")
+print("HELLOOOO3")
 scope = SpotifyOAuth(scope=scope, username=username)
+print("HELLOOOO4")
 #spotify = spotipy.Spotipy(auth_manager=token)
 
 
 spotifyObject.user_playlist_create(user=username, name=playlist_name, public=True, description=playlist_description)
+print("HELLOOOO5")
 # user_input = ''
 # user_input = input('Enter songs\' name: ')
 # list_of_songs = []
