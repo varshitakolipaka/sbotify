dbddd1c92b08e3fa826cb5f7266bd454eed07a64
gaurav-singh-2003 <gaurav.san.kot@gmail.com>
2021-07-06 04:25:10 +0530

    set command functionality

--- bot.py
+++ bot.py
@@ -1,25 +1,19 @@
 # import the discord library
 import discord
 from discord import message
+from discord import embeds
 from createplaylist import main
+from commands_parser import *
 import array
 # bot connect
 client = discord.Client()
 # this variable stores id of an output channel, you can have many variables corresponding to different channels
 out_channel = 854832431764602901
+prefix = '!'
 
-# list of commands
-commands = ["add", "remove"]
-
-# checks if a given command is in the list or not
-
-
-def is_command(command):
-    if command in commands:
-        return 1
-    else:
-        return 0
-
+# list of playlists
+playlists = [[''badmin2'', ["yo", "lmao"],
+              ["another yo", "another lmao"]], ["7536042733445921155"]]
 # event refers to any new,.. well .... event xD, like sending a message,a reaction, a reply, a bot joining etc
 
 
@@ -39,16 +33,52 @@ async def on_message(message):
     if input_mssg[0] == '!':
         mssg = input_mssg.split(" ", 1)
         command = mssg[0][1:len(mssg[0])]
-        if is_command(command):
+        if is_command_add(command):
             query = mssg[1]
-            main(query,"https://open.spotify.com/playlist/123gKtvvoFi0iNMjvTHsC3")
+            # main(query,"https://open.spotify.com/playlist/123gKtvvoFi0iNMjvTHsC3")
             # await output_channel.send(command)
             # await output_channel.send(mssg[1])
+            await output_channel.send(message.author.id)
+        elif is_command_set(command):
+            flag1 = 0
+            flag2 = 0
+            if(len(mssg) == 1):
+                myEmbed = discord.Embed(
+                    title="Error", description="No argument specified.\n Try {prefix} help to see all commands")
+                await output_channel.send(embed=myEmbed)
+            for i in playlists:
+                if(i[0] == str(message.author.id)):
+                    # print(i[0])
+                    if (len(i) != 1):
+                        # print("yess")
+                        for j in i:
+                            if(type(j) == str):
+                                continue
+                            if(j[0] == mssg[1]):
+                                pos = i.index(j)
+                                swap(i, 1, pos)
+                                flag2 = 1
+                                break
+                        print("lmao")
+                        flag1 = 1
+                        break
+                        print(i[0])
+
+            if(flag1 == 0):
+                myEmbed = discord.Embed(
+                    title="Error", description="Uh Oh :(( , you are not yet part of the clan\n Use {prefix} join to be a part of noise_bot family!")
+                await output_channel.send(embed=myEmbed)
+            elif(flag2 == 0):
+                # make_playlist(mssg[1])
+                myEmbed = discord.Embed(
+                    title="Playlist created", description="Name : {mssg[1]}")
+                await output_channel.send(embed=myEmbed)
+            for i in playlists:
+                print(i)
         else:
             myEmbed = discord.Embed(
-                title="Error", description="This is not a recognized command.\n Try <prefix>help to see all commands")
+                title="Error", description="This is not a recognized command.\n Try {prefix} help to see all commands")
             await output_channel.send(embed=myEmbed)
-            # yp = 0
 
 
 # Run the client on this server
---
+++ commands_parser.py
@@ -0,0 +1,30 @@
+# checks if a given command is in the list or not
+# list of commands
+def swap(list, pos1, pos2):
+
+    list[pos1], list[pos2] = list[pos2], list[pos1]
+    return list
+
+
+commands = ["add", "remove", "set"]
+
+
+def is_command(command):
+    if command in commands:
+        return 1
+    else:
+        return 0
+
+
+def is_command_add(command):
+    if(is_command(command) and command == "add"):
+        return 1
+    else:
+        return 0
+
+
+def is_command_set(command):
+    if(is_command(command) and command == "set"):
+        return 1
+    else:
+        return 0
