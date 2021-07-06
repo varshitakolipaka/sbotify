b81a669be57be6fdada0205ff4efc88a47254ef7
gaurav-singh-2003 <gaurav.san.kot@gmail.com>
2021-07-06 14:49:21 +0530

    playlists management

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ from discord import message
 from discord import embeds
 from createplaylist import main
 from commands_parser import *
-import array
 # bot connect
 client = discord.Client()
 # this variable stores id of an output channel, you can have many variables corresponding to different channels
@@ -12,9 +11,9 @@ out_channel = 854832431764602901
 prefix = '!'
 
 # list of playlists
-playlists = [[''badmin2'', ["yo", "lmao"],
-              ["another yo", "another lmao"]], ["7536042733445921155"]]
+playlists = []
 # event refers to any new,.. well .... event xD, like sending a message,a reaction, a reply, a bot joining etc
+members = []
 
 
 @client.event
@@ -39,6 +38,20 @@ async def on_message(message):
             # await output_channel.send(command)
             # await output_channel.send(mssg[1])
             await output_channel.send(message.author.id)
+        elif is_command_join(command):
+            new_member = [str(message.author.id)]
+            if str(message.author.id) in members:
+                myEmbed = discord.Embed(
+                    title="You are already a part of the clan", description="Try {prefix} help to see all commands")
+                await output_channel.send(embed=myEmbed)
+            else:
+                playlists.append(new_member)
+                members.append(str(message.author.id))
+                myEmbed = discord.Embed(
+                    title="Welcome to the noise bot pawri!", description="Try {prefix} help to see all commands")
+                await output_channel.send(embed=myEmbed)
+            print(playlists)
+            print(members)
         elif is_command_set(command):
             flag1 = 0
             flag2 = 0
@@ -46,30 +59,31 @@ async def on_message(message):
                 myEmbed = discord.Embed(
                     title="Error", description="No argument specified.\n Try {prefix} help to see all commands")
                 await output_channel.send(embed=myEmbed)
-            for i in playlists:
-                if(i[0] == str(message.author.id)):
-                    # print(i[0])
-                    if (len(i) != 1):
-                        # print("yess")
-                        for j in i:
-                            if(type(j) == str):
-                                continue
-                            if(j[0] == mssg[1]):
-                                pos = i.index(j)
-                                swap(i, 1, pos)
-                                flag2 = 1
-                                break
-                        print("lmao")
-                        flag1 = 1
-                        break
-                        print(i[0])
+            if str(message.author.id) in members:
+                flag1 = 1
+                for i in playlists:
+                    if(i[0] == str(message.author.id)):
+                        # print(i[0])
+                        if (len(i) != 1):
+                            # print("yess")
+                            for j in i:
+                                if(type(j) == str):
+                                    continue
+                                if(j[0] == mssg[1]):
+                                    pos = i.index(j)
+                                    swap(i, 1, pos)
+                                    flag2 = 1
+                                    break
+                            break
 
             if(flag1 == 0):
                 myEmbed = discord.Embed(
                     title="Error", description="Uh Oh :(( , you are not yet part of the clan\n Use {prefix} join to be a part of noise_bot family!")
                 await output_channel.send(embed=myEmbed)
             elif(flag2 == 0):
-                # make_playlist(mssg[1])
+                await output_channel.send("Playlist doesn't exist.\nDo you want to create one by the name {mssg[1]}\n Reply with (y/n) to this message")
+
+                playlists[playlists.index(i)].append([mssg[1], 'NULL'])
                 myEmbed = discord.Embed(
                     title="Playlist created", description="Name : {mssg[1]}")
                 await output_channel.send(embed=myEmbed)
