e0e29c95a37838dd5dfd9c54f59e17ff8fc2089a
varshitakolipaka <varshita2003@gmail.com>
2021-07-18 18:37:27 +0400

    lol

--- database/db_mgmt.py
+++ database/db_mgmt.py
@@ -12,10 +12,8 @@ class DB:
         return [i[0] for i in self.cur.description], self.cur.fetchall()
 
 
-playlist_db = DB('playlist.db')
-# playlist_db.cur.execute('''
-# CREATE TABLE Members (member_id VARCHAR, set_playlist VARCHAR)''')
-playlist_db.cur.execute('''
+sbotify_db = DB('playlist.db')
+sbotify_db.cur.execute('''
 INSERT INTO Members(member_id, set_playlist)
 VALUES
     ('012','o'),
@@ -27,21 +25,69 @@ VALUES
     ('567','f'),
     ('678','g')
 ''')
+
+sbotify_db.cur.execute('''
+INSERT INTO Playlists(playlist_name, playlist_id)
+VALUES
+    ('poo','1'),
+    ('boo','2'),
+    ('voo','3'),
+    ('coo','4'),
+    ('doo','5'),
+    ('soo','6'),
+    ('qoo','7')
+''')
+# sbotify_db.cur.execute('''
+# CREATE TABLE Members (member_id VARCHAR, set_playlist VARCHAR)''')
+# sbotify_db.cur.execute('''
+# CREATE TABLE Playlists (playlist_name VARCHAR, playlist_id VARCHAR)''')
+
+# MEMBERS TABLE
+
+
 def check_member(member_id):
-    col, res = playlist_db.run_query(f'''
+    col, res = sbotify_db.run_query(f'''
     SELECT EXISTS(SELECT DISTINCT * FROM Members WHERE member_id='{member_id}')''')
     flag = res[0][0]
     return flag
-flag1=check_member('678')
-print(flag1)
-def update_set_playlist(member):
-    col, res = playlist_db.run_query(f'''
-    UPDATE Members SET set_playlist = '{playlist_name}' WHERE member_id='{member_id}'
+
+
+flag1 = check_member('678')
+# print(flag1)
+
+
+def update_set_playlist(member_id, playlist_id):
+    sbotify_db.cur.execute(f'''
+    UPDATE Members SET set_playlist = '{playlist_id}' WHERE member_id='{member_id}'
      ''')
 
-update_set_playlist = 
 
-# col, res = playlist_db.run_query("SELECT * FROM Members")
-# print(res)
+# PLAYLIST TABLE
+# check if playlist exists
+def check_playlist(playlist_name):
+    col, res = sbotify_db.run_query(f'''
+    SELECT EXISTS(SELECT DISTINCT * FROM Playlists WHERE playlist_name='{playlist_name}')''')
+    flag = res[0][0]
+    return flag
 
-# playlist_db.db.commit()
+
+def return_playlist_id(playlist_name):
+    if check_playlist(playlist_name) == 1:
+        col, res = sbotify_db.run_query(f'''
+        SELECT playlist_id FROM Playlists WHERE playlist_name='{playlist_name}'
+        ''')
+        return res[0][0]
+    else:
+        return None
+
+
+flag2 = return_playlist_id('doo')
+# def create_playlist
+print(flag2)
+# Printing the tbale contents
+col, res = sbotify_db.run_query("SELECT * FROM Members")
+print(res)
+col, res = sbotify_db.run_query("SELECT * FROM Playlists")
+print(res)
+
+# sbotify_db.db.commit()
