7e08f26e05de46193b9ddf8ce1636c639f145dec
varshitakolipaka <varshita2003@gmail.com>
2021-07-18 17:18:00 +0400

    lol

--- database/db_mgmt.py
+++ database/db_mgmt.py
@@ -1,1 +1,41 @@
 import sqlite3
+
+
+class DB:
+    def __init__(self, path):
+        self.path = path
+        self.db = sqlite3.connect(self.path)
+        self.cur = self.db.cursor()
+
+    def run_query(self, query):
+        self.cur.execute(query)
+        return [i[0] for i in self.cur.description], self.cur.fetchall()
+
+
+playlist_db = DB('playlist.db')
+# playlist_db.cur.execute('''
+# CREATE TABLE Members (member_id VARCHAR, top_playlist VARCHAR)''')
+playlist_db.cur.execute('''
+INSERT INTO Members(member_id, top_playlist)
+VALUES
+    ('012','o'),
+    ('123','a'),
+    ('234','b'),
+    ('234','c'),
+    ('345','d'),
+    ('456','e'),
+    ('567','f'),
+    ('678','g')
+''')
+def check_member(member_id):
+    col, res = playlist_db.run_query(f'''
+    SELECT EXISTS(SELECT DISTINCT * FROM Members WHERE member_id='{member_id}')''')
+    flag = res[0][0]
+    return flag
+flag1=check_member('678')
+print(flag1)
+
+# col, res = playlist_db.run_query("SELECT * FROM Members")
+# print(res)
+
+# playlist_db.db.commit()
